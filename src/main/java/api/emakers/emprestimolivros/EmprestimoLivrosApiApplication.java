3863aecaf333111ff8eb1d2aab1d7322d034f3fa
Davi Pedro <davisilvaps02@gmail.com>
2024-04-23 15:25:57 -0300

    projeto base

---
+++ src/main/java/api/emakers/emprestimolivros/EmprestimoLivrosApiApplication.java
@@ -0,0 +1,13 @@
+package api.emakers.emprestimolivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoLivrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoLivrosApiApplication.class, args);
+	}
+
+}
