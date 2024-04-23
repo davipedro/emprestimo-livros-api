174a6ed0894258057b0aec560e81c0e4ec40dd7d
Davi Pedro <davisilvaps02@gmail.com>
2024-04-23 16:54:30 -0300

    Adicionando end-point GET todos os livros

--- src/main/java/api/emakers/emprestimolivros/controller/LivroController.java
+++ src/main/java/api/emakers/emprestimolivros/controller/LivroController.java
@@ -3,8 +3,28 @@ package api.emakers.emprestimolivros.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import api.emakers.emprestimolivros.model.Livro;
+import api.emakers.emprestimolivros.service.LivroService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
+
+    @Autowired
+    private LivroService livroService;
     
+    @GetMapping
+    public ResponseEntity<List<Livro>> buscarTodosLivros() {
+        List<Livro> livros = livroService.buscarTodosLivros();
+        
+        return ResponseEntity.ok().body(livros);
+    }
 }
--- src/main/java/api/emakers/emprestimolivros/service/LivroService.java
+++ src/main/java/api/emakers/emprestimolivros/service/LivroService.java
@@ -1,8 +1,21 @@
 package api.emakers.emprestimolivros.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import api.emakers.emprestimolivros.model.Livro;
+import api.emakers.emprestimolivros.repository.LivroRepository;
 
 @Service
 public class LivroService {
+    
+    @Autowired
+    private LivroRepository livroRepository;
+
+    public List<Livro> buscarTodosLivros() {
+        return livroRepository.findAll();
+    }
     
 }
