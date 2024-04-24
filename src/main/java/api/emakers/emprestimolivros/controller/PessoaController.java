75fefdbbcb24eae90a182499ae357f98c6960a24
davipedro <davisilvaps02@gmail.com>
2024-04-23 22:55:20 -0300

    Adicionando endpoint GET e POST para buscar por id e cadastrar pessoa

--- src/main/java/api/emakers/emprestimolivros/controller/PessoaController.java
+++ src/main/java/api/emakers/emprestimolivros/controller/PessoaController.java
@@ -3,14 +3,21 @@ package api.emakers.emprestimolivros.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import api.emakers.emprestimolivros.Dto.pessoa.PostPessoa;
 import api.emakers.emprestimolivros.model.Pessoa;
 import api.emakers.emprestimolivros.service.PessoaService;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @RestController
@@ -26,6 +33,22 @@ public class PessoaController {
         List<Pessoa> pessoas = pessoaService.buscarTodasPessoas();
 
         return ResponseEntity.ok().body(pessoas);
+    }
+    
+    @GetMapping("/pessoa/{id}")
+    public ResponseEntity<Pessoa> buscarPessoaPorId(@PathVariable Long id) {
+        
+        Pessoa pessoa = pessoaService.buscarPessoaPorId(id);
+
+        return ResponseEntity.ok().body(pessoa);
+    }
+    
+    @PostMapping("/pessoa")
+    public ResponseEntity<Pessoa> cadastrarPessoa(@RequestBody @Valid PostPessoa dado) {
+        
+        pessoaService.cadastrarPessoa(dado);
+
+        return ResponseEntity.noContent().build();
     }
     
     
--- src/main/java/api/emakers/emprestimolivros/service/PessoaService.java
+++ src/main/java/api/emakers/emprestimolivros/service/PessoaService.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import api.emakers.emprestimolivros.Dto.pessoa.PostPessoa;
+import api.emakers.emprestimolivros.infra.exceptions.PessoaNaoEncontradaException;
+import api.emakers.emprestimolivros.model.Endereco;
 import api.emakers.emprestimolivros.model.Pessoa;
 import api.emakers.emprestimolivros.repository.PessoaRepository;
+import jakarta.validation.Valid;
 
 @Service
 public class PessoaService {
@@ -16,6 +20,31 @@ public class PessoaService {
 
     public List<Pessoa> buscarTodasPessoas() {
         return pessoaRepository.findAll();
+    }
+
+    public Pessoa buscarPessoaPorId(Long id) {
+        return pessoaRepository.findById(id)
+        .orElseThrow(() -> new PessoaNaoEncontradaException());
+    }
+
+    public void cadastrarPessoa(PostPessoa dado) {
+
+        var endereco = new Endereco(
+            dado.logradouro(),
+            dado.bairro(),
+            dado.cep(),
+            dado.numero(),
+            dado.complemento(),
+            dado.cidade(),
+            dado.uf()
+        );
+
+        Pessoa pessoa = new Pessoa(
+            dado.nome(),
+            endereco
+        );
+
+        pessoaRepository.save(pessoa);
     }
     
 }
