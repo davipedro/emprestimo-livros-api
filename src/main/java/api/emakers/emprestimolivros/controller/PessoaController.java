88ef26e2f6bb8d2df81a2ae1eebb16ccff0f0d83
davipedro <davisilvaps02@gmail.com>
2024-04-23 22:22:55 -0300

    Adicionando endpoint GET para listar todas as pessoas

--- src/main/java/api/emakers/emprestimolivros/controller/PessoaController.java
+++ src/main/java/api/emakers/emprestimolivros/controller/PessoaController.java
@@ -3,8 +3,30 @@ package api.emakers.emprestimolivros.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import api.emakers.emprestimolivros.model.Pessoa;
+import api.emakers.emprestimolivros.service.PessoaService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
+    
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> buscarTodasPessoas() {
+
+        List<Pessoa> pessoas = pessoaService.buscarTodasPessoas();
+
+        return ResponseEntity.ok().body(pessoas);
+    }
+    
     
 }
--- src/main/java/api/emakers/emprestimolivros/service/PessoaService.java
+++ src/main/java/api/emakers/emprestimolivros/service/PessoaService.java
@@ -1,8 +1,21 @@
 package api.emakers.emprestimolivros.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import api.emakers.emprestimolivros.model.Pessoa;
+import api.emakers.emprestimolivros.repository.PessoaRepository;
 
 @Service
 public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public List<Pessoa> buscarTodasPessoas() {
+        return pessoaRepository.findAll();
+    }
     
 }
