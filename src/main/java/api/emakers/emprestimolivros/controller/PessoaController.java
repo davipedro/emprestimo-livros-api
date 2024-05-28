bcd857db85c02f316708f5c3997cebd80baf33fc
davipedro <davisilvaps02@gmail.com>
2024-05-28 15:04:59 -0300

    Adicionando pegarLivroEmprestado e buscarLivrosPessoa

--- src/main/java/api/emakers/emprestimolivros/controller/PessoaController.java
+++ src/main/java/api/emakers/emprestimolivros/controller/PessoaController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import api.emakers.emprestimolivros.Dto.pessoa.PostPessoa;
 import api.emakers.emprestimolivros.Dto.pessoa.UpdatePessoa;
+import api.emakers.emprestimolivros.model.Livro;
 import api.emakers.emprestimolivros.model.Pessoa;
+import api.emakers.emprestimolivros.service.LivroService;
 import api.emakers.emprestimolivros.service.PessoaService;
 import jakarta.validation.Valid;
 
@@ -13,6 +15,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +23,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
+
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
 
     @Autowired
     private PessoaService pessoaService;
+
+    @Autowired
+    private LivroService livroService;
+
+    @Autowired
     
     @GetMapping
     public ResponseEntity<List<Pessoa>> buscarTodasPessoas() {
@@ -65,6 +74,23 @@ public class PessoaController {
         pessoaService.deletarPessoa(id);
         
         return ResponseEntity.noContent().build();
+    }
+
+    @Transactional
+    @PutMapping("/pessoa/{pessoaId}/emprestimo/{livroId}")
+    public ResponseEntity<Object> pegarLivroEmprestado(@PathVariable Long pessoaId, @PathVariable Long livroId) {
+        
+        livroService.emprestimoLivro(pessoaId, livroId);
+        
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/pessoa/{id}/livros")
+    public ResponseEntity<List<Livro>> buscarLivrosPessoa(@PathVariable Long id) {
+        
+        var livros = pessoaService.buscarLivrosPessoa(id);
+        
+        return ResponseEntity.ok().body(livros);
     }
     
 }
