d882a531cb58c3faa1222bbdeb4df4172e163df6
davipedro <davisilvaps02@gmail.com>
2024-05-28 15:11:34 -0300

    Adicionando interface PessoaValidador

---
+++ src/main/java/api/emakers/emprestimolivros/infra/validacoes/PessoaValidador.java
@@ -0,0 +1,8 @@
+package api.emakers.emprestimolivros.infra.validacoes;
+
+import api.emakers.emprestimolivros.model.Pessoa;
+
+public interface PessoaValidador {
+    
+    public void validar(Pessoa pessoa);
+}
