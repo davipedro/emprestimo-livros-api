d83dfff45cc1cdd87efbb6c478be2a8d30b616a5
davipedro <davisilvaps02@gmail.com>
2024-05-28 15:11:14 -0300

    Adicionando interface para validações

---
+++ src/main/java/api/emakers/emprestimolivros/infra/validacoes/LivroValidador.java
@@ -0,0 +1,8 @@
+package api.emakers.emprestimolivros.infra.validacoes;
+
+import api.emakers.emprestimolivros.model.Livro;
+
+public interface LivroValidador {
+    
+    public void validar(Livro livro);
+}
