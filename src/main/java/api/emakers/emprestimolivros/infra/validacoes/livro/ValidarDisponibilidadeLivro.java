27e359bd33e9f6b79c5aa4ad7877f2bec08e5b2f
davipedro <davisilvaps02@gmail.com>
2024-05-28 15:11:52 -0300

    Adicionando validação de disponibilidade de livro

---
+++ src/main/java/api/emakers/emprestimolivros/infra/validacoes/livro/ValidarDisponibilidadeLivro.java
@@ -0,0 +1,17 @@
+package api.emakers.emprestimolivros.infra.validacoes.livro;
+
+import org.springframework.stereotype.Component;
+
+import api.emakers.emprestimolivros.infra.exceptions.LivroIndisponivelException;
+import api.emakers.emprestimolivros.infra.validacoes.LivroValidador;
+import api.emakers.emprestimolivros.model.Livro;
+
+@Component
+public class ValidarDisponibilidadeLivro implements LivroValidador{
+    @Override
+    public void validar(Livro livro) {
+        if(livro.getQuantidade() == 0) {
+            throw new LivroIndisponivelException("Todos os livros já foram emprestados");
+        }
+    }
+}
