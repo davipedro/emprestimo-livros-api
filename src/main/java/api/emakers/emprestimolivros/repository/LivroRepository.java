d29f86bb92d9389c9ce76cb95d8b6a745869ef01
Davi Pedro <davisilvaps02@gmail.com>
2024-05-28 19:03:20 -0300

    Corrigindo dominio da consulta findLivrosByPessoaId

--- src/main/java/api/emakers/emprestimolivros/repository/LivroRepository.java
+++ src/main/java/api/emakers/emprestimolivros/repository/LivroRepository.java
@@ -1,9 +1,16 @@
 package api.emakers.emprestimolivros.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import api.emakers.emprestimolivros.model.Livro;
 
 public interface LivroRepository extends JpaRepository<Livro, Long>{
     
+    @Query("SELECT l FROM Pessoa p JOIN p.livros l WHERE p.id = :pessoaId")
+    Optional<List<Livro>> findLivrosByPessoaId(@Param("pessoaId") Long pessoaId);
 }
